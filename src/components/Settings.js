c1bcc5152fdf2204ec6018e97b7224190a2f5c56
akash plackal <akashplackal@gmail.com>
2021-10-24 21:50:22 +0530

    added a dialog box

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { BsGearFill } from "react-icons/bs";
 
@@ -7,13 +7,48 @@ const SettingIcon = styled(BsGearFill)`
   z-index: 10;
   font-size: 1.2rem;
   position: absolute;
-  left: 0.4rem;
-  bottom: 0.4rem;
+  left: 2rem;
+  bottom: 1rem;
   opacity: 0.8;
 `;
 
+const DialogMenu = styled.div`
+  background-color: whitesmoke;
+  height: 30rem;
+  width: 39.5rem;
+  z-index: 10;
+  position: absolute;
+  transition: 0.2s ease-in-out;
+  left: ${({ toggle }) => (toggle ? "1rem" : "-100%")};
+  bottom: 3rem;
+  border-radius: 0.7rem;
+  opacity: ${({ toggle }) => (toggle ? "1" : "0")};
+`;
+
+const TinyDiv = styled.div`
+  transform: rotate(0.4turn);
+  background-color: whitesmoke;
+  width: 1rem;
+  height: 1rem;
+  position: absolute;
+  transition: 0.2s ease-in-out;
+  left: ${({ toggle }) => (toggle ? "36px" : "-100%")};
+  bottom: 42px;
+  z-index: 10;
+  opacity: ${({ toggle }) => (toggle ? "1" : "0")};
+`;
+
 const Settings = () => {
-  return <SettingIcon onClick={() => console.log("hello")} />;
+  const [toggle, setToggle] = useState(false);
+  const changeState = () => setToggle(!toggle);
+
+  return (
+    <>
+      <DialogMenu toggle={toggle} />
+      <TinyDiv toggle={toggle} />
+      <SettingIcon onClick={changeState} />
+    </>
+  );
 };
 
 export default Settings;
